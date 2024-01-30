9e7ddf4a56ff2f44509134cf696d0cfc746ab586
StoneHF <94833939+StoneHF@users.noreply.github.com>
2024-01-30 00:04:59 +0000

    Add files via upload

--- admin_home_page.php
+++ admin_home_page.php
@@ -3,9 +3,14 @@
 <?php
 session_start();
 include 'config/conn.php';
-include 'adminCheck/navbarCheck.php';
+include 'AdminNav/adNavbar.php';
 
-$varMemberID = $_SESSION['fldMemberID'];
+// Check if the user is logged in and is an admin
+if (!isset($_SESSION['fldMemberID']) || !isset($_SESSION['isAdmin']) || $_SESSION['isAdmin'] != 1) {
+  // If not logged in or not an admin, redirect to login page
+  header('Location: login_form.php');
+  exit();
+}
 
 ?>
 
