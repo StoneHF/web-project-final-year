2c700c6e7488a1ca23c68abd0c59823e259a85f6
StoneHF <94833939+StoneHF@users.noreply.github.com>
2024-02-23 18:01:26 +0000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,20 @@
+<!-- footer.php -->
+<style>
+    body {
+        margin-bottom: 70px; /* Adjust the value based on your preference */
+    }
+
+    footer {
+        background-color: black;
+        color: white;
+        padding: 20px;
+        text-align: center;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
+
+<footer>
+    <p>Welcome to GameNTech!</p>
+</footer>
